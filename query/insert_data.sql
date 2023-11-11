5b62e7404a2ea619f0c2ec290bfe9561984fb7f3
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-11-11 11:13:41 +0700

    insert data table customer

---
+++ query/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO customer(name, gender, phone, address)
+VALUES('Amalia Septiani', 'Perempuan', '081234567891', 'Jl. in aja dulu'),
+('Amalia', 'Perempuan', '089326518264', 'Jl. sm mantan'),
+('Septi', 'Perempuan', '082834691735', 'Jl. yuk'),
+('Siti', 'Perempuan', '089734256482', 'Jl. kemana'),
+('Sarah', 'Perempuan', '0893426378134', 'Jl. sm yang lain'),
+('Icha', 'Perempuan', '086351827364', 'Jl. sm aku aja'),
+('Doni', 'Laki-laki', '082537481627', 'Jl. aja terus'),
+('Rani', 'Perempuan', '088253647581', 'Jl. pulang'),
+('James', 'Laki-laki', '0854928351672', 'Jl. kesini'),
+('William', 'Laki-laki', '08925361748', 'Jl. yu besok');
