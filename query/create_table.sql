a241720fb3f4f1bf3caf769ca0b13a0a482fa7a7
AmaliaSeptiani <amalia2003septiani@students.unnes.ac.id>
2023-11-11 11:01:58 +0700

    create table varian

--- query/create_table.sql
+++ query/create_table.sql
@@ -6,3 +6,7 @@ phone VARCHAR (15) not null,
 address VARCHAR (100) not NULL
 ) ENGINE = InnoDB;
 
+CREATE TABLE varian(
+id VARCHAR(5) PRIMARY KEY NOT NULL,
+name ENUM('Laptop', 'Smartphone', 'Aksesoris', 'ATK') NOT NULL
+) ENGINE = InnoDB;
